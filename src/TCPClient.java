f992719b1c30f637b3da164e79aa94364e871477
Ray <rxrosario1117@gmail.com>
2023-02-10 13:26:16 -0500

    Added resultBW.close() to make the Buffered Writer write to the results.txt file //Ray

--- src/TCPClient.java
+++ src/TCPClient.java
@@ -9,7 +9,8 @@
            BufferedReader in = null; // for reading form ServerRouter
            InetAddress addr = InetAddress.getLocalHost();
 		   String localHost = addr.getHostAddress(); // Client machine's IP
-      	   String routerName = "192.168.1.76"; // ServerRouter localHost name
+//      	   String routerName = "192.168.1.77"; // ServerRouter localHost name
+      	   String routerName = "10.74.24.162"; // ServerRouter localHost name (KSU pc)
 		   int SockNum = 5555; // port number
 
 			// Tries to connect to the ServerRouter
@@ -27,7 +28,7 @@
                System.err.println("Couldn't get I/O for the connection to: " + routerName);
                System.exit(1);
             }
-				
+
       	// Variables for message passing	
          Reader reader = new FileReader("./file.txt");
 			BufferedReader fromFile =  new BufferedReader(reader); // reader for the string file
@@ -36,6 +37,8 @@
 //			String address =routerName; // destination IP (Server)
 			String address ="192.168.1.67"; // destination IP (Server)
 			long t0, t1, t;
+            //Variables for calculations
+           int msgCount = 0, totalMsgSize = 0, totalTransmissionSize = 0;
 			
 			// Communication process (initial sends/receives
 			out.println(address);// initial send (IP of the destination Server)
@@ -52,12 +55,15 @@
                  break;
 
              t = t1 - t0;
+             totalTransmissionSize += t;
              System.out.println("Cycle time: " + t);
           
              fromUser = fromFile.readLine(); // reading strings from a file
              if (fromUser != null) {
                  System.out.println("Client: " + fromUser);
                  out.println(fromUser); // sending the strings to the Server via ServerRouter
+                 msgCount++; //Incrementing message count
+                 totalMsgSize += fromUser.length(); //adding length of message to totalMsgSize
                  t0 = System.currentTimeMillis();
              }
          }
@@ -66,5 +72,25 @@
          out.close();
          in.close();
          Socket.close();
-      }
+
+         double avgMessageSize = 0, avgTransmissionTime = 0, avgLookUpTime = 0;
+         if (msgCount > 0) {
+             avgMessageSize = (double) totalMsgSize/msgCount; //Calculating average message size
+             avgTransmissionTime = (double) totalTransmissionSize/msgCount;
+         }
+
+         //Writing statistical results to results.txt
+         try {
+             FileWriter resultFW = new FileWriter("results.txt");
+             BufferedWriter resultBW = new BufferedWriter(resultFW);
+             resultBW.write("Average message size is: " + avgMessageSize);
+             resultBW.newLine();
+             resultBW.write("Average transmission time is: "+avgTransmissionTime);
+             resultBW.close();
+
+
+         } catch (IOException e) {
+             System.out.println("Error writing to file: "+e.getMessage());
+         }
+       }
    }
